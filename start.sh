3d440a01856d38ba5c18556531acab869945b0e3
acmol <xianxiake@163.com>
2017-12-31 22:00:36 +0800

    1. do not do balance if have no Open/Close 2. do not change to next coin_type if have no Open/Close

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@
 # Created Time: Wed 27 Dec 2017 09:57:21 PM CST
 #########################################################################
 #!/bin/bash
-sh stop.sh 2> logs/stop.err
+sh stop.sh 2> log/stop.err
 nohup python3 $PWD/trading.py > log/trading.out 2> log/trading.err &
 sh start_monitor.sh
